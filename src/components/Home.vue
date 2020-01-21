b6f2cf7a3559ce5d76a16cd7533c03dbd562a83b
utdallas-atec <jermaine.fyi@gmail.com>
2020-01-21 15:30:16 -0600

    made change to test webhook

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="home">
-		<h1>This is the home page of the Cherwell Support app.</h1>
+		<h1>This is the home page of the Cherwell Support app. It's blank. There is no app yet.</h1>
 		<h2>(but there's nothing here)</h2>
 	</div>
 </template>
